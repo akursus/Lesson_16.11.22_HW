a1636bccd850864ed623a70dc5a9c6987d164483
akursus <ak_ursus@list.ru>
2022-11-21 21:58:29 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1
+        System.out.println("Задание 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке");
     }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else
+            System.out.println("Возврат на стартовую ссылку для скачивания приложения");
+
+        }
 }
