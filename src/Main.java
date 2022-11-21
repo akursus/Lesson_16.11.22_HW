1489792c350948d4fd1308b6d3de198d390752f1
akursus <ak_ursus@list.ru>
2022-11-21 22:32:51 +0300

    Task 1 changes 1

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         System.out.println("Задание 1");
         int clientOS = 0;
         if (clientOS == 0) {
-        System.out.println("Установите версию приложения для iOS по ссылке");
-    }
-        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
-        else
+        else {
             System.out.println("Возврат на стартовую ссылку для скачивания приложения");
-
+        }
         }
 }
