d1022698c717a345d65de47d9e31d0b7185818ef
akursus <ak_ursus@list.ru>
2022-11-21 23:14:18 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,24 @@ public class Main {
         else {
             System.out.println("Возврат на стартовую ссылку для скачивания приложения");
         }
+        //Task 2
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2012;
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else  if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Возврат на стартовую ссылку для скачивания приложения");
         }
+
+    }
 }
